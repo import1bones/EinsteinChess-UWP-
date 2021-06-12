02a23497128065a349af1b4faa59588e7d46d8a9
mute <mute@DESKTOP-KSTSB5U>
2021-06-12 13:12:19 +0800

    添加项目文件。

---
+++ EinsteinChess/Cell.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinsteinChess
+{
+    public class Cell
+    {
+        public string FillColor { get; set; }
+        public int ChessManNo { get; set; }
+        public Boolean IsChessMan { get; set; }
+    }
+}
