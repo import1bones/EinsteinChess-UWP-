f4a2374ef6cc415178da52863a728232413829b1
YanChao <13519377850@163.com>
2021-06-19 23:02:10 +0800

    inital and finish some gui

--- EinsteinChess/Cell.cs
+++ EinsteinChess/Cell.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EinsteinChess
+﻿namespace EinsteinChess
 {
     public class Cell
     {
         public string FillColor { get; set; }
         public int ChessManNo { get; set; }
-        public Boolean IsChessMan { get; set; }
+        public bool IsChessMan { get; set; }
+        public string IsVisibility { get; set; }
     }
 }
--- EinsteinChess/MainPage.xaml.cs
+++ EinsteinChess/MainPage.xaml.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.ComponentModel;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -21,22 +10,65 @@ namespace EinsteinChess
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
         //Cell => Cells obj
-        private ObservableCollection<Cell> Cells { get; set;  }
+        private ObservableCollection<Cell> Cells { get; set; }
             = new ObservableCollection<Cell>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string ChessManToVisibility(bool isChessMan) => isChessMan ? "Visible" : "Collapsed";
 
         public MainPage()
         {
-            this.InitializeComponent();
-            this.Cells.Add(new Cell() { FillColor = "Red", ChessManNo = 6, IsChessMan = true });
-            this.Cells.Add(new Cell() { FillColor = "Red", ChessManNo = 5, IsChessMan = true });
-            this.Cells.Add(new Cell() { FillColor = "Red", ChessManNo = 4, IsChessMan = true });
-            this.Cells.Add(new Cell() { FillColor = "Red", ChessManNo = 6, IsChessMan = true });
-            this.Cells.Add(new Cell() { FillColor = "Red", ChessManNo = 5, IsChessMan = true });
-            this.Cells.Add(new Cell() { FillColor = "Red", ChessManNo = 4, IsChessMan = true });
+            InitializeComponent();
+            for (int i = 0; i < 25; ++i)
+            {
+                Cells.Add(new Cell() { FillColor = "White", ChessManNo = 0, IsChessMan = false, IsVisibility= "Collapsed" });
+            }
+
+            int[,] test = {
+                {1,2,3,0,0 },
+                {4,5,0,0,0 },
+                {6,0,0,0,-1 },
+                {0,0,0,-2,-3 },
+                {0,0,-4,-5,-6 } };
+            UpdateCells(test);
         }
 
+        
+
+        private void UpdateCells(int[,] chessBoard)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+
+                    if (chessBoard[x, y] < 0)
+                    {
+
+                        Cells[(x * 5) + y].FillColor = "Red";
+                        Cells[(x * 5) + y].ChessManNo = Math.Abs(chessBoard[x, y]);
+                        Cells[(x * 5) + y].IsChessMan = true;
+                    }
+                    else if (chessBoard[x, y] > 0)
+                    {
+                        Cells[(x * 5) + y].FillColor = "Blue";
+                        Cells[(x * 5) + y].ChessManNo = Math.Abs(chessBoard[x, y]);
+                        Cells[(x * 5) + y].IsChessMan = true;
+                    }
+                    else
+                    {
+                        Cells[(x * 5) + y].FillColor = "White";
+                        Cells[(x * 5) + y].ChessManNo = 0;
+                        Cells[(x * 5) + y].IsChessMan = false;
+                    }
+                    Cells[(x * 5) + y].IsVisibility = ChessManToVisibility(Cells[(x * 5) + y].IsChessMan);
+                }
+            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Cells)));
+        }
     }
 }
